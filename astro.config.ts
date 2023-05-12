be4a2edf1d793916ae4c0debc9e736fc278a9ae8
Thuan Pham <thuanowa@gmail.com>
2023-05-12 10:29:42 +0700

    quickfix

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
@@ -19,7 +18,6 @@ export default defineConfig({
     },
   },
   integrations: [
-    mdx({}),
     tailwind({
       config: {
         applyBaseStyles: false,
